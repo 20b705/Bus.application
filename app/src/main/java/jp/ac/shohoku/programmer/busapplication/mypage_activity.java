2da8b4d93db119a974f3a13a2136cd8a5436fe6b
s20b813 <s20b813@shohoku.ac.jp>
2021-11-12 12:34:09 +0900

    Webviewを表示させる変更を加えた。

--- app/src/main/java/jp/ac/shohoku/programmer/busapplication/mypage_activity.java
+++ app/src/main/java/jp/ac/shohoku/programmer/busapplication/mypage_activity.java
@@ -3,6 +3,8 @@ package jp.ac.shohoku.programmer.busapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class mypage_activity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class mypage_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage_activity);
+        WebView myWebView = (WebView) findViewById(R.id.webView); //謎のエラー
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("https://sites.google.com/a/shohoku.ac.jp/portal/\n" +
+                "\n");
     }
 }
