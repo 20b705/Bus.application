77337f3a077b92d16a71215be7d9f57cf688e0d9
kazuhiro kakinuma <s20b705@shohoku.ac.jp>
2021-11-12 10:31:48 +0900

    新たなアクティビティの作成

---
+++ app/src/main/java/jp/ac/shohoku/programmer/busapplication/school_activity.java
@@ -0,0 +1,14 @@
+package jp.ac.shohoku.programmer.busapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class school_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_school_activity);
+    }
+}
