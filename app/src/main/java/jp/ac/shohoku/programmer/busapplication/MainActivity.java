2ba1d020874ee88ae3bf2b2ebe393b6c00871469
kazuhiro kakinuma <s20b705@shohoku.ac.jp>
2021-11-19 12:30:55 +0900

    Home.java時計の作成、画面遷移の作成

--- app/src/main/java/jp/ac/shohoku/programmer/busapplication/MainActivity.java
+++ app/src/main/java/jp/ac/shohoku/programmer/busapplication/MainActivity.java
@@ -2,7 +2,12 @@ package jp.ac.shohoku.programmer.busapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button nextButton = findViewById(R.id.schoolBus_button);
+        Button nettButton2 = findViewById(R.id.KanaBus_button);
+        Button nextButton3 = findViewById(R.id.schoolBus_button2);
+        Button nextButton4 = findViewById(R.id.KanaBus_button2);
+        Button nextButton5 = findViewById(R.id.Home_button);
+        Button nextButton6 = findViewById(R.id.schoolBus_button3);
+        Button nextButton7 = findViewById(R.id.KanaBus_button3);
+        Button nextButton8 = findViewById(R.id.Mypage_button);
+
     }
 }
--- app/src/main/java/jp/ac/shohoku/programmer/busapplication/mypage_activity.java
+++ app/src/main/java/jp/ac/shohoku/programmer/busapplication/mypage_activity.java
@@ -8,13 +8,13 @@ import android.webkit.WebViewClient;
 
 public class mypage_activity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mypage_activity);
-        WebView myWebView = (WebView) findViewById(R.id.webView); //謎のエラー
-        myWebView.setWebViewClient(new WebViewClient());
-        myWebView.loadUrl("https://sites.google.com/a/shohoku.ac.jp/portal/\n" +
-                "\n");
-    }
+    //@Override
+    //protected void onCreate(Bundle savedInstanceState) {
+        //super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_mypage_activity);
+        //WebView myWebView = (WebView) findViewById(R.id.webView); //謎のエラー
+        //myWebView.setWebViewClient(new WebViewClient());
+        //myWebView.loadUrl("https://sites.google.com/a/shohoku.ac.jp/portal/\n" +
+          //      "\n");
+    //}
 }
