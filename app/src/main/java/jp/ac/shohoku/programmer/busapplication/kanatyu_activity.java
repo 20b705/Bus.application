380f1b97d24220d0823890c47d91fc9f9c42eedb
kazuhiro kakinuma <s20b705@shohoku.ac.jp>
2021-12-03 12:42:31 +0900

    Home.java画面遷移の作成

--- app/src/main/java/jp/ac/shohoku/programmer/busapplication/kanatyu_activity.java
+++ app/src/main/java/jp/ac/shohoku/programmer/busapplication/kanatyu_activity.java
@@ -15,5 +15,46 @@ public class kanatyu_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kanatyu_activity);
+        Button nextButton_Kana1 = findViewById(R.id.Kana_home_button);
+        Button nextButton_Kana2 = findViewById(R.id.Kana_School_button);
+        Button nextButton_Kana3 = findViewById(R.id.Kana_Kanachu_button);
+        Button nextButton_Kana4 = findViewById(R.id.Kana_mypage_button);
+
+        nextButton_Kana1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), MainActivity.class);
+                //EditText editText = findViewById(R.id.editText)
+                startActivity(intent);
+            }
+        });
+
+        nextButton_Kana2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), school_activity.class);
+                //EditText editText = findViewById(R.id.editText)
+                startActivity(intent);
+            }
+        });
+
+        nextButton_Kana3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), kanatyu_activity.class);
+                //EditText editText = findViewById(R.id.editText)
+                startActivity(intent);
+            }
+        });
+
+        nextButton_Kana4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), mypage_activity.class);
+                //EditText editText = findViewById(R.id.editText)
+                startActivity(intent);
+            }
+        });
     }
+
 }
