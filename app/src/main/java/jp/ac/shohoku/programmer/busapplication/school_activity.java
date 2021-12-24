2cf358931563d62b9f279e9213fcd9d1826ea505
kazuhiro kakinuma <s20b705@shohoku.ac.jp>
2021-12-24 12:26:27 +0900

    school_activity.javaのデータベースの作成

--- app/src/main/java/jp/ac/shohoku/programmer/busapplication/school_activity.java
+++ app/src/main/java/jp/ac/shohoku/programmer/busapplication/school_activity.java
@@ -1,13 +1,22 @@
 package jp.ac.shohoku.programmer.busapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.ContentValues;
+//import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class school_activity extends AppCompatActivity {
+    private TextView textView;
+    private SchoolDataOpenHelper SchoolHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +59,50 @@ public class school_activity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
 
+        //DB作成
+        SchoolHelper = new SchoolDataOpenHelper(getApplicationContext());
+        textView = findViewById(R.id.SchoolBus_Data);
+    }
+    public  void readData(View view){
+        SQLiteDatabase db = SchoolHelper.getReadableDatabase();
+        Cursor cursor =db.query(
+                "Schooldb",
+                new String[]{ "title", "score" },
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+
+        cursor.moveToFirst();
+
+        StringBuilder sbuilder = new StringBuilder();
+
+        for (int i = 0; i < cursor.getCount(); i++) {
+            sbuilder.append(cursor.getString(0));
+            sbuilder.append(":    ");
+            sbuilder.append(cursor.getInt(1));
+            sbuilder.append("点\n\n");
+            cursor.moveToNext();
+        }
+
+        cursor.close();
+
+        textView.setText(sbuilder.toString());
+    }
+    public void saveData(View view){
+        SQLiteDatabase db = SchoolHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        //EditText editTextTitle = findViewById(R.id.edit_text_key);
+        //EditText editTextScore = findViewById(R.id.edit_text_value);
+        //String title = editTextTitle.getText().toString();
+        //String score = editTextScore.getText().toString();
+        //values.put("title", title);
+        //values.put("score", score);
+
+        db.insert("testdb", null, values);
+    }
 }
